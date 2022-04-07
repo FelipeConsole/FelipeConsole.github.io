079671fccc1dfc189d6265c25963a12ca8117823
FelipeConsole <felipe.console@usp.br>
2022-04-07 00:23:29 -0300

    first commit

---
+++ db/db.js
@@ -0,0 +1,314 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"Hi &#128075;",
+				"I'm Vinay Somawat. I’m currently working as a software engineer@Novopay specialized in JavaScript Applications. I did my undergrad in CS from NIT Warangal.",
+				"I'm a developer, geek and curious human besides being an OpenSource enthusiast. I have experience of delivering solutions for interesting problems from startup and industry space.",
+				"I love to read, listen to music, and travel.",
+			],
+		},
+		contact: {
+			text: [
+				"If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email. My email address is: vinaysomawat40@gmail.com",
+			],
+		},
+	},
+	skills: [
+		{
+			title: "Languages",
+			skillName: "Python",
+			color: "1",
+			percentage: "100",
+		},
+		
+		{
+			title: "Statistica Analysis",
+			skillName: " Statistical Analysis",
+			color: "2",
+			percentage: "100",
+		},
+		
+		{
+			title: "Backend",
+			skillName: "Git",
+			color: "3",
+			percentage: "100",
+		},
+		{
+			title: "ML",
+			skillName: "Machine Learning",
+			color: "4",
+			percentage: "100",
+		},
+		// {
+		// 	title: "Design",
+		// 	skillName: "HTML, Bootstrap, SCSS",
+		// 	color: "4",
+		// 	percentage: "70",
+		// },
+		// {
+		// 	title: "Version Control",
+		// 	skillName: "GitHub, JIRA, Trello",
+		// 	color: "7",
+		// 	percentage: "70",
+		// },
+		// {
+		// 	title: "Tools",
+		// 	skillName: "Postman, Chrome DevTools",
+		// 	color: "3",
+		// 	percentage: "80",
+		// },
+		// {
+		// 	title: "Saas products",
+		// 	skillName: "CleverTap, FreshDesk",
+		// 	color: "5",
+		// 	percentage: "50",
+		// },
+		// {
+		// 	title: "Editor",
+		// 	skillName: "VS Code",
+		// 	color: "6",
+		// 	percentage: "100",
+		// }, 
+		
+	],
+	projects: {
+		web: [
+			// {
+			// 	projectName: "Programming Diaries",
+			// 	image: "images/programmingdiaries.png",
+			// 	summary:
+			// 		"Developed a full stack blog application to provide content on techical topics across the internet with admin interface.",
+			// 	preview: "https://programmingdiaries.herokuapp.com/",
+			// 	techStack: [
+			// 		"Django",
+			// 		"SQLite",
+			// 		"Bootstrap",
+			// 		"JavaScript",
+			// 		"Heroku",
+			// 	],
+			// },
+			// {
+			// 	projectName: "Find Your Bank",
+			// 	image: "images/findyourbank.png",
+			// 	summary:
+			// 		"Developed a React application to render a list of banks fetched from API. Filtered the banks based on queries from localstorage, marked favorites banks.",
+			// 	preview: "https://clever-fermi-0d5d76.netlify.app",
+			// 	techStack: ["Reactjs", "Bootstrap", "JavaScript", "Netlify"],
+			// },
+			// {
+			// 	projectName: "Web Portfolio",
+			// 	image: "images/portfolio.png",
+			// 	summary:
+			// 		"Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
+			// 	preview: "https://github.com/vinaysomawat/Travographer-Portal",
+			// 	techStack: ["HTML", "Bootstrap", "JavaScript"],
+			// },
+			// {
+			// 	projectName: "Resume Builder",
+			// 	image: "images/resume-builder.png",
+			// 	summary:
+			// 		"Browser based editor to build and download Resumes in a customizable templates.",
+			// 	preview: "https://vinaysomawat.github.io/Resume-Builder",
+			// 	techStack: ["HTML", "Bootstrap", "JavaScript"],
+			// },
+		],
+		
+		software: [
+			// {
+			// 	projectName: "Pizza Ordering ChatBot",
+			// 	image: "images/pizzaorderchatbot.png",
+			// 	summary:
+			// 		"ChatBot using Dialogflow, Firebase database which stores the chat data in the realtime database.",
+			// 	preview:
+			// 		"https://github.com/vinaysomawat/Pizza-Ordering-ChatBot",
+			// 	techStack: ["Dailogflow", "Firebase"],
+			// },
+			// {
+			// 	projectName: "WhatsApp-Bot",
+			// 	image: "images/whatsappbot.jpg",
+			// 	summary:
+			// 		"Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
+			// 	preview: "https://github.com/vinaysomawat/WhatsApp-Bot",
+			// 	techStack: ["Selenium", "Chrome Webdriver", "Python"],
+			// },
+			// {
+			// 	projectName: "Bill Generator",
+			// 	image: "images/billgenerator.png",
+			// 	summary:
+			// 		"GUI to transfer data to excel sheets and generate bills on the local shops.",
+			// 	preview: "https://github.com/vinaysomawat/Bill-Generator",
+			// 	techStack: ["Tkinter", "Openxlpy", "Python"],
+			// },
+		],
+		android: [
+			{
+				projectName: "Classification",
+				image: "images/titanic.jpg",
+				summary:
+					"Classification for the Titanic dataset.",
+				preview: "https://github.com/FelipeConsole/classification",
+				techStack: ["Python", "Machine Learning", "Classification"],
+			},
+			{
+				projectName: "Risk Minimization for Portfolio Management",
+				image: "images/rescaled_risk_portifolio.jpg",
+				summary:
+					"Minimizink risk for portifolio stocks.",
+				preview: "https://github.com/FelipeConsole/Playing_with_stocks",
+				techStack: ["Julia", "Stocks", "Risk Management"],
+			},
+		],
+		freelance: [
+			// {
+			// 	projectName: "SnylloAir.com",
+			// 	image: "images/snylloair.png",
+			// 	summary:
+			// 		"Developed a company website to showcase the purpose, services and products provided by the company to audience.",
+			// 	preview: "https://www.snylloair.com/",
+			// 	techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
+			// },
+			// {
+			// 	projectName: "Delivery+",
+			// 	image: "images/AM-Logo-.png",
+			// 	summary:
+			// 		"Android Application to display website in android devices.",
+			// 	preview:
+			// 		"https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
+			// 	techStack: ["Android", "JAVA", "Play Store"],
+			// },
+		],
+	},
+	experience: [
+		 {
+			title: "Novopay Solutions Pvt. Ltd.",
+			duration: "June 2020 - Present",
+			subtitle: "Software Engineer, SDE Intern",
+			details: [
+				"Supporting both frontend & backend teams for AEPS, DMT transaction, CMS Service, gateways.",
+				"Working on biometric eKYCs, CDM card and onboarding retailers flows.",
+			],
+			tags: [
+				"JavaScript",
+				"Angular",
+				"React",
+				"Bootstrap",
+				"Nodejs",
+				"Jenkins",
+			],
+			icon: "qrcode",
+		  }, 
+		
+		{
+			title: "ThinkPedia LLP",
+			duration: "May 2019 - June 2019",
+			subtitle: "SDE Intern",
+			details: [
+				"Worked as a full stack developer to support tech team.",
+				"Developed a customer Web Application from scratch for social media management.",
+			],
+			tags: ["JavaScript", "Angular", "Bootstrap", "Java", "Spring Boot"],
+			icon: "group",
+		},
+	],
+	education: [
+		{
+			title: "Bachelor of Science in Physics",
+			duration: "2013-2016",
+			subtitle: "University of São Paulo",
+			// details: ["Black Holes"],
+			tags: [
+				"Group Theory",
+				"Lie Groups/Algebras",
+				"General Relativity"
+			],
+			icon: "graduation-cap",
+		},
+		{
+			title: "Master of Science in Physics",
+			duration: "2017-2019",
+			subtitle: "University of São Paulo",
+			details: ["Black Holes"],
+			tags: [
+				" General Relativity",
+				"Black Holes",
+				"Anti-de Sitter Spacetime",
+				"Scalar-Tensor Theoris of Gravity",
+			],
+			icon: "graduation-cap",
+		},
+		{
+			title: "PhD in Physics",
+			duration: "2019-2023",
+			subtitle: "University of São Paulo",
+			details: [""],
+			tags: [
+				" General Relativity",
+				"Boson Stars",
+				"Black Holes",
+			],
+			icon: "graduation-cap",
+		},
+	],
+	footer: [
+		{
+			label: "Research",
+			data: [
+				{
+					text: "iNSPIRE",
+					link: "https://inspirehep.net/authors/1828432",
+				},
+				{
+					text: "Google Scholar",
+					link: "https://scholar.google.com.br/citations?hl=pt-BR&user=u2Omkw0AAAAJ&view_op=list_works&gmla=AJsN-F5SOWPSGyiEYkL_jps5S881iC2vy51oK6hwstrUrUzplhGbX49-aQJ8oCwYNVEEJx19qSGfqnAG_AHfhOdihLK-ViGpyvWQaHellLPwyxqQ70KSS4U",
+				},
+				{
+					text: "ArXiv",
+					link: "https://arxiv.org/search/?searchtype=author&query=C%C3%B4nsole%2C+F",
+				},
+			],
+		},
+		// {
+		// 	label: "Resources",
+		// 	data: [
+		// 		{
+		// 			text: "Enable Dark/Light Mode",
+		// 			func: "enableDarkMode()",
+		// 		},
+		// 		{
+		// 			text: "Print this page",
+		// 			func: "window.print()",
+		// 		},
+		// 		{
+		// 			text: "Clone this page",
+		// 			link: "https://github.com/FelipeConsole",
+		// 		},
+		// 	],
+		// },
+		{
+			label: "Social Media",
+			data: [
+				{
+					text: "Linkedin",
+					link: "https://www.linkedin.com/in/felipe-console-01317a166/",
+				},
+				{
+					text: "Twitter",
+					link: "https://twitter.com/consolelipe",
+				},
+				// {
+				// 	text: "Buy me a coffee",
+				// 	link: "https://www.buymeacoffee.com/r194dME8y",
+				// },
+			],
+		},
+		// {
+		// 	label: "copyright-text",
+		// 	data: [
+		// 		"Made with &hearts; by Vinay Somawat.",
+		// 		"&copy; No Copyrights. Feel free to use this template."
+		// 	],
+		// },
+	],
+};
